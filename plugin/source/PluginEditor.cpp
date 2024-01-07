f08031285929903b1a4957184605014c9222ba57
owendostie <owendostie@gmail.com>
2024-01-07 13:49:18 -0500

    Small fixes

--- plugin/source/PluginEditor.cpp
+++ plugin/source/PluginEditor.cpp
@@ -1,6 +1,10 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+TextButton playButton          { "Play" };
+TextButton openButton          { "Open" };
+TextButton stopButton          { "Stop" };
+
 //==============================================================================
 KingpinAudioProcessorEditor::KingpinAudioProcessorEditor (KingpinAudioProcessor& p)
     : AudioProcessorEditor (&p), processorRef (p)
@@ -9,6 +13,22 @@ KingpinAudioProcessorEditor::KingpinAudioProcessorEditor (KingpinAudioProcessor&
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (800, 600);
+
+    addAndMakeVisible (&openButton);
+    openButton.setButtonText ("Open...");
+    // openButton.onClick = [this] { openButtonClicked(); };
+
+    addAndMakeVisible (&playButton);
+    playButton.setButtonText ("Play");
+    // playButton.onClick = [this] { playButtonClicked(); };
+    playButton.setColour (juce::TextButton::buttonColourId, juce::Colours::green);
+    playButton.setEnabled (true);
+
+    addAndMakeVisible (&stopButton);
+    stopButton.setButtonText ("Stop");
+    // stopButton.onClick = [this] { stopButtonClicked(); };
+    stopButton.setColour (juce::TextButton::buttonColourId, juce::Colours::red);
+    stopButton.setEnabled (true);
 
     // AudioFileReaderComponent<ConvolutionDemoDSP> fileReaderComponent;
 }
